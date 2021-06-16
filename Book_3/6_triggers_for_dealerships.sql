53bfa5dd29af3c52a2929ad13340ba9d8c08f7a1
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-06-16 16:01:40 -0700

    url function

---
+++ Book_3/6_triggers_for_dealerships.sql
@@ -0,0 +1,16 @@
+--Because Carnival is a single company, we want to ensure that there is consistency in the data provided to the user.
+--Each dealership has it's own website but we want to make sure the website URL are consistent and easy to remember.
+--Therefore, any time a new dealership is added or an existing dealership is updated,
+--we want to ensure that the website URL has the following format: http://www.carnivalcars.com/{name of the dealership with underscores separating words}.
+
+CREATE or replace FUNCTION set_dealership_url()
+  RETURNS TRIGGER
+  LANGUAGE PlPGSQL
+AS $$
+BEGIN
+  UPDATE dealerships
+  SET website = CONCAT('http://www.carnivalcars.com/', new.business_name)
+  where dealerships.dealership_id = new.dealership_id;
+  RETURN NULL;
+END;
+$$
