5e5abff5b1231d9acf751706de32725d291c31c5
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-06-22 21:26:55 -0700

    create function

--- Book_3/5_triggers_intro.sql
+++ Book_3/5_triggers_intro.sql
@@ -1,1 +1,15 @@
 --Create a trigger for when a new Sales record is added, set the pickup date to 7 days from the purchase date.
+
+CREATE FUNCTION set_pickup_date() 
+  RETURNS TRIGGER 
+  LANGUAGE PlPGSQL
+AS $$
+BEGIN
+  -- trigger function logic
+  UPDATE sales
+  SET pickup_date = NEW.purchase_date + integer '7'
+  WHERE sales.sale_id = NEW.sale_id;
+  
+  RETURN NULL;
+END;
+$$
