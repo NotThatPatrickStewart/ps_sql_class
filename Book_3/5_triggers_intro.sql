2efe1c3d7901484fbc98f0c77cbb535faf6189c7
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-06-22 21:27:51 -0700

    insert values and test

--- Book_3/5_triggers_intro.sql
+++ Book_3/5_triggers_intro.sql
@@ -19,3 +19,8 @@ CREATE TRIGGER new_sale_made
   ON sales
   FOR EACH ROW
   EXECUTE PROCEDURE set_pickup_date();
+
+  insert into sales (sales_type_id, vehicle_id, employee_id, customer_id, dealership_id, price, deposit, purchase_date, invoice_number, payment_method, sale_returned)
+ 	values (1, 10000, 497, 834, 27, 27000, 3000, '2021-06-07', '4684684465', 'mastercard', false);
+ 	
+select * from sales where invoice_number = '4684684465';
