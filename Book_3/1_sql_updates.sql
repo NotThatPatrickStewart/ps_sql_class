135f04f456ad266d30ad12ed3761cead4b9c8c11
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-06-25 14:58:02 -0700

    question 1/2

---
+++ Book_3/1_sql_updates.sql
@@ -0,0 +1,3 @@
+--Kristopher Blumfield an employee of Carnival has asked to be transferred to a different dealership location.
+--She is currently at dealership 9. She would like to work at dealership 20. Update her record to reflect her transfer.
+
