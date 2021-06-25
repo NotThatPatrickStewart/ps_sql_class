539c62c5309c4821c8572cc15b631f318ad0b5e9
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-06-25 14:58:42 -0700

    question 2/2

--- Book_3/1_sql_updates.sql
+++ Book_3/1_sql_updates.sql
@@ -12,3 +12,7 @@ where employee_id =
 
 select * from dealershipemployees d 
 where employee_id = 9;
+
+
+--A Sales associate needs to update a sales record because her customer want to pay with a Mastercard instead of JCB.
+--Update Customer, Ernestus Abeau Sales record which has an invoice number of 9086714242.
