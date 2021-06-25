cb9f4cd70f5c5967b420ac30f7d71dac7275ed9e
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-06-25 14:58:24 -0700

    answer 1/2

--- Book_3/1_sql_updates.sql
+++ Book_3/1_sql_updates.sql
@@ -1,3 +1,14 @@
 --Kristopher Blumfield an employee of Carnival has asked to be transferred to a different dealership location.
 --She is currently at dealership 9. She would like to work at dealership 20. Update her record to reflect her transfer.
 
+update dealershipemployees
+set dealership_id = 20
+where employee_id = 9;
+
+update dealershipemployees
+set dealership_id = 20
+where employee_id =
+(select employee_id from employees where first_name = 'Kristopher' and last_name = 'Blumfield');
+
+select * from dealershipemployees d 
+where employee_id = 9;
