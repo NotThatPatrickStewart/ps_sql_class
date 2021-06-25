a49d3cc6544a877fa5da7bc2b5888873740d1a87
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-06-24 22:15:14 -0700

    declare variables

---
+++ Book_3/8_carnival_vehicles_transactions.sql
@@ -0,0 +1,10 @@
+--Adding 5 brand new 2021 Honda CR-Vs to the inventory. They have I4 engines and are classified as a Crossover SUV or CUV. 
+--All of them have beige interiors but the exterior colors are Lilac, Dark Red, Lime, Navy and Sand.
+--The floor price is $21,755 and the MSR price is $18,999.
+
+do $$
+declare
+	newvehicletype int;
+	newmake int;
+	newmodel int;
+	newtypeid int;
